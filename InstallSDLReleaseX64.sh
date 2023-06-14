89eb4ca89c6e7381584689ff261188600cc84e29
drgmitev <drgmitev@gmail.com>
2023-06-15 01:32:23 +0300

    feat: shell scripts to build sdl in vendored folder

---
+++ InstallSDLReleaseX64.sh
@@ -0,0 +1,6 @@
+rm -rf "./engine/libraries/SDL/build" &&
+rm -rf "./engine/libraries/SDL/install" &&
+cd engine/libraries/SDL &&
+cmake -DCMAKE_CXX_FLAGS=-m64 -DCMAKE_SHARED_LINKER_FLAGS=-m64 -S . -B ./build &&
+cmake --build ./build --config Release &&
+cmake --install ./build --prefix ./install --config Release 
