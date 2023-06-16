17a2903c23f5d8b2866b559e067cd08fccd4cf21
drgmitev <drgmitev@gmail.com>
2023-06-16 20:36:26 +0300

    fix: delete x86 scripts as I cannot test them

--- InstallSDLDebugX32.bat
+++
@@ -1,8 +0,0 @@
-cd engine/libraries/SDL
-cmake -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -S . -B ./build
-cmake --build ./build --config Debug
-cmake --install ./build --prefix ./install --config Debug
-cd ../SDL_Image
-cmake -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 -DSDL3_DIR=../SDL/install/cmake/ -S . -B ./build
-cmake --build ./build --config Debug 
-cmake --install ./build --prefix ./install --config Debug
