e5770e91e3bc46259588da817809e36154fab585
drgmitev <drgmitev@gmail.com>
2023-06-18 16:24:21 +0300

    [WIP] feat (linux-config): box2d

--- linux_config.cmake
+++ linux_config.cmake
@@ -1,2 +1,4 @@
- set(SDL3_DIR "${ENGINE_ROOT}/libraries/SDL/build")
- set(SDL3_INCLUDE_DIRS "${ENGINE_ROOT}/libraries/SDL/install/include")  
+set(SDL3_DIR "${ENGINE_ROOT}/libraries/SDL/build")
+set(SDL3_INCLUDE_DIRS "${ENGINE_ROOT}/libraries/SDL/install/include")
+set(box2d_DIR "${ENGINE_ROOT}/libraries/box2d/install/lib/cmake/box2d")
+set(box2d_INCLUDE_DIRS "${ENGINE_ROOT}/libraries/box2d/install/include")
