05bf74ab4f133a51cd6946b715e021bb8406668c
drgmitev <drgmitev@gmail.com>
2023-06-18 14:09:21 +0300

    feat!(utils): forgotten file from previous commit

--- engine/utils/entry.h
+++ engine/utils/entry.h
@@ -1,15 +1,32 @@
 #pragma once
 #include "layer_stack.h"
 namespace utils {
+    /**
+     * @brief Entry point of an application
+     * @note All implementations of this class should handle
+     * their runtime. \n End of `run` should end the
+     * lifetime of the running applicatioin
+     *
+     */
     class Entry {
       public:
         Entry() = default;
-        virtual ~Entry(){
+        virtual ~Entry(){};
 
-        };
+        /**
+         * @brief All continous logic of this `Entry`
+         *
+         */
         virtual void run() = 0;
 
       protected:
+        /**
+         * @brief An application should be able to run
+         * several layers at oncem and call their events
+         * consecutively
+         * @see utils::LayerStack
+         *
+         */
         LayerStack layerStack;
     };
 } // namespace utils
