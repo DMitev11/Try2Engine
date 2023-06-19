772df9cbc3474a283cf924187db745167f0ae679
drgmitev <drgmitev@gmail.com>
2023-06-19 21:51:24 +0300

    fix: delete binaries and build of box2d before regenerating

--- InstallBox2dDebug.bat
+++ InstallBox2dDebug.bat
@@ -1,4 +1,6 @@
 cd ./engine/libraries/box2d
+rd /s /q "./install"
+rd /s /q "./build"
 cmake -S . -B ./build
 cmake --build ./build --config Debug
 cmake --install ./build --prefix ./install --config Debug
