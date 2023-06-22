b9e3f93429d1aa319f02af9a615a9658c31a6f58
drgmitev <drgmitev@gmail.com>
2023-06-23 02:11:29 +0300

    feat! (examples): create a window

---
+++ examples/CreateAWindow.cpp
@@ -0,0 +1,17 @@
+#include <loader/loader.hpp>
+#include <render/render.hpp>
+#include <window/window.hpp>
+
+int main(int argc, char const *argv[]) {
+    // 1. Initialize windowing - parameter list based on the
+    // implementation. Check build generative flags (CMake
+    // flags) in the README.md
+    window::init();
+    // 2. Create a window
+    auto window = window::createWindow(1000, 1000, "try1");
+    // 3. Terminate/Destroy a window
+    window::terminate(window);
+    // 4. Terminate/Destroy windowing library when all
+    // windows are done
+    window::shutdown();
+}
