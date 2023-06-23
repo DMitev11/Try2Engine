f45077c2fe685fa3abbf83cdda65347cd63cae14
drgmitev <drgmitev@gmail.com>
2023-06-23 12:58:40 +0300

    feat(tests): placeholder tests

---
+++ engine/utils/tests/event_emitter.cc
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+#include <utils/event_emitter.hpp>
+TEST(EventEmitter, Create) { EXPECT_EQ(1, 1); }
---
+++ tests/utils/event_emitter.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+TEST(RND, RND)
+{
+    EXPECT_EQ(1, 1);
+}
